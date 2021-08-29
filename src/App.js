314c8975af8ca065f7bbb22e97032a3d00bddd1f
Stefania Popa <stefania_florina10@yahoo.com>
2021-08-29 12:44:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [growth, setGrowth] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <header className='App-header'>
+       <h2>Use Effect</h2>
+       <h3>growth:{growth}</h3>
+     </header>
+   </div>
   );
 }
 
